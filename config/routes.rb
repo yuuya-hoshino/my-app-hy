3ef6c3f03052d077dd9f14558b0eb492b52c9422
yuuya-hoshino <65810632+yuuya-hoshino@users.noreply.github.com>
2020-11-08 14:24:13 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
+
+  resources :items, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
